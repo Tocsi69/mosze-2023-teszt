2300078a4e0cd8edf561e129ca174651345779a5
Barna Mészáros <meszaros.bbbbarni@gmail.com>
2024-09-05 20:31:24 +0200

    Javított kód

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //hibás változó
-    std::cout << '1-100 ertekek duplazasa' //rossz szintaktika
-    for (int i = 0;) //pontosvessző rossz helyen
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:" //rossz szintaktika
+        std::cout << "Ertek:";
     }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //pontosvessző helyett vessző
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] //szintaktika hiba
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
